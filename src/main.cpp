1e0c797f5fd3f40f6aea510d56b56b2c9fab0fe0
Mictronics <github@mictronics.de>
2024-03-10 09:44:25 +0100

    Added debug output for clock speed results.

--- src/main.cpp
+++ src/main.cpp
@@ -219,6 +219,25 @@ void setup()
 
     LOG_INFO("\n\n//\\ E S H T /\\ S T / C\n\n");
 
+    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+
+    LOG_INFO("Clock speed:\n");
+    LOG_INFO("pll_sys  = %dkHz\n", f_pll_sys);
+    LOG_INFO("pll_usb  = %dkHz\n", f_pll_usb);
+    LOG_INFO("rosc     = %dkHz\n", f_rosc);
+    LOG_INFO("clk_sys  = %dkHz\n", f_clk_sys);
+    LOG_INFO("clk_peri = %dkHz\n", f_clk_peri);
+    LOG_INFO("clk_usb  = %dkHz\n", f_clk_usb);
+    LOG_INFO("clk_adc  = %dkHz\n", f_clk_adc);
+    LOG_INFO("clk_rtc  = %dkHz\n", f_clk_rtc);
+
     initDeepSleep();
 
     // Testing this fix für erratic T-Echo boot behaviour
