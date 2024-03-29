a986db650f2f63082e4e9a7287e256438f844a8e
Mictronics <github@mictronics.de>
2024-03-29 09:51:41 +0100

    Fixed log output. Time is uint32_t.

--- src/mesh/NodeDB.h
+++ src/mesh/NodeDB.h
@@ -136,11 +136,11 @@ class NodeDB
     void setLocalPosition(meshtastic_Position position, bool timeOnly = false)
     {
         if (timeOnly) {
-            LOG_DEBUG("Setting local position time only: time=%i\n", position.time);
+            LOG_DEBUG("Setting local position time only: time=%u\n", position.time);
             localPosition.time = position.time;
             return;
         }
-        LOG_DEBUG("Setting local position: latitude=%i, longitude=%i, time=%i\n", position.latitude_i, position.longitude_i,
+        LOG_DEBUG("Setting local position: latitude=%i, longitude=%i, time=%u\n", position.latitude_i, position.longitude_i,
                   position.time);
         localPosition = position;
     }
