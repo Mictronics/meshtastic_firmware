6fd85f0ab1211430408dd7f79abd388d4ea4bab6
Mictronics <github@mictronics.de>
2025-11-23 10:40:20 +0100

    Removed interval scaling since I know what I'm doing.

--- src/mesh/Default.cpp
+++ src/mesh/Default.cpp
@@ -37,15 +37,7 @@ uint32_t Default::getConfiguredOrDefault(uint32_t configured, uint32_t defaultVa
  */
 uint32_t Default::getConfiguredOrDefaultMsScaled(uint32_t configured, uint32_t defaultValue, uint32_t numOnlineNodes)
 {
-    // If we are a router, we don't scale the value. It's already significantly higher.
-    if (config.device.role == meshtastic_Config_DeviceConfig_Role_ROUTER)
-        return getConfiguredOrDefaultMs(configured, defaultValue);
-
-    // Additionally if we're a tracker or sensor, we want priority to send position and telemetry
-    if (IS_ONE_OF(config.device.role, meshtastic_Config_DeviceConfig_Role_SENSOR, meshtastic_Config_DeviceConfig_Role_TRACKER))
-        return getConfiguredOrDefaultMs(configured, defaultValue);
-
-    return getConfiguredOrDefaultMs(configured, defaultValue) * congestionScalingCoefficient(numOnlineNodes);
+    return getConfiguredOrDefaultMs(configured, defaultValue);
 }
 
 uint32_t Default::getConfiguredOrMinimumValue(uint32_t configured, uint32_t minValue)
