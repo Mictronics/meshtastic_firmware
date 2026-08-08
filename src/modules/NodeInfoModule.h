39fda249860294f27c51c85360c31bf9a343b488
Mictronics <github@mictronics.de>
2026-08-08 09:50:50 +0200

    Fixed double declaration.

--- src/modules/NodeInfoModule.h
+++ src/modules/NodeInfoModule.h
@@ -50,7 +50,6 @@ class NodeInfoModule : public ProtobufModule<meshtastic_User>, private concurren
   private:
     uint32_t lastSentToMesh = 0; // Last time we sent our NodeInfo to the mesh
     bool shorterTimeout = false;
-    uint32_t lastSentToMesh = 0; // Last time we sent our NodeInfo to the mesh
     bool suppressReplyForCurrentRequest = false;
     std::map<NodeNum, uint32_t> lastNodeInfoSeen;
 
