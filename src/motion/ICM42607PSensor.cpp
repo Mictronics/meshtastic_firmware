156b3595c9a7f75108997c7a2824a6cf3ffef646
Mictronics <github@mictronics.de>
2026-08-08 09:51:05 +0200

    Removed useless code.

--- src/motion/ICM42607PSensor.cpp
+++ src/motion/ICM42607PSensor.cpp
@@ -71,26 +71,6 @@ int32_t ICM42607PSensor::runOnce()
     }
     return MOTION_SENSOR_CHECK_INTERVAL_MS;
 #else
-    int16_t x = 0;
-    int16_t y = 0;
-    int16_t z = 0;
-    inv_imu_sensor_event_t event = {};
-
-    if (sensor == nullptr || sensor->getDataFromRegisters(event) != 0) {
-        return MOTION_SENSOR_CHECK_INTERVAL_MS;
-    }
-
-    // getDataFromRegisters() fills accel[] but does not set sensor_mask in this library version.
-    if (event.accel[0] == 0 && event.accel[1] == 0 && event.accel[2] == 0) {
-        return MOTION_SENSOR_CHECK_INTERVAL_MS;
-    }
-
-    x = event.accel[0];
-    y = event.accel[1];
-    z = event.accel[2];
-    // LOG_DEBUG("ICM-42607-P accel read x=%.3fg y=%.3fg z=%.3fg", (float)x / ICM42607P_COUNTS_PER_G,
-    //           (float)y / ICM42607P_COUNTS_PER_G, (float)z / ICM42607P_COUNTS_PER_G);
-
     return MOTION_SENSOR_CHECK_INTERVAL_MS;
 #endif
 }
